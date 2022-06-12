62a263ff1174bab37e35fd8aa0cd679d896ec911
bambamxx <oaj.padillasolis@ugto.mx>
2022-06-12 11:50:56 -0500

    resolucion fondo negro navbar y estilos fichero section integrado en home

--- script.js
+++ script.js
@@ -43,3 +43,85 @@ point.forEach((eachPoint, i) => {
 
     })
 })
+
+
+
+//Formulario registro
+function register() {
+    const user = document.getElementById("user").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirmation = document.getElementById("passwordConfirmation").value;
+
+//4
+    if (user == "" && email == "" && password == "" && passwordConfirmation == "") {
+        alert("Favor de ingresar usuario, correo, la contraseña y su confirmación");
+    }
+//3
+    else if (email == "" && password == "" && passwordConfirmation == "") {
+        alert("Favor de ingresar correo, la contraseña y su confirmación");
+    }
+
+    else if (user == "" && password == "" && passwordConfirmation == "") {
+        alert("Favor de ingresar usuario, la contraseña y su confirmación");
+    }
+
+    else if (user == "" && email == "" && passwordConfirmation == "") {
+        alert("Favor de ingresar usuario, correo y confirmar la contraseña");
+    }
+
+    else if (user == "" && email == "" && password == "") {
+        alert("Favor de ingresar usuario, correo y contraseña");
+    }
+
+//2
+    else if (user == "" && email == "") {
+        alert("Favor de ingresar usuario y correo");
+    }
+
+    else if (user == "" && password == "") {
+        alert("Favor de ingresar usuario y contraseña");
+    }
+
+    else if (user == "" && passwordConfirmation == "") {
+        alert("Favor de ingresar usuario y la confirmación de la contraseña");
+    }
+
+    else if (email == "" && password == "") {
+        alert("Favor de ingresar correo y contraseña");
+    }
+
+    else if (email == "" && passwordConfirmation == "") {
+        alert("Favor de ingresar correo y la confirmación de la contraseña");
+    }
+//1
+    else if (user == "" ) {
+        alert("Usuario vacio");
+    }
+
+    else if (email == "") {
+        alert("Correo vacio");
+    }
+
+    else if (password == "") {
+        alert("Contraseña vacia");
+    }
+
+    else if (password.length < 6) {
+        alert("La contraseña debe contener al menos 6 caracteres"); 
+    }
+
+    else if (password.length >= 6) {
+
+        if (passwordConfirmation == "") {
+            alert("Favor de confirmar la contraseña");
+        }   
+        
+        else if (passwordConfirmation === password && user != "" && email != ""){
+            alert("Registro exitoso");
+        }
+        else {
+            alert("Las contraseñas no coinciden")
+        }
+    }
+}
